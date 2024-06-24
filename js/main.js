300884b2f07d230f73fe983c128693974a038dc8
WdHeIvTiEl-13 <atikshahriar060806@gmail.com>
2024-06-24 19:27:09 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -59,12 +59,12 @@ profile_btn.addEventListener('click', profile_Box);
 
 
 
-const chat_btn10 = document.getElementById('btn10');
+const chat_b =document.getElementById('bt');
 
-function chat_Box10() {
+function chat() {
   const x = document.createElement("div");
   x.classList.add("add");
   document.body.append(x);
   x.innerHTML = "<input><button>Send</button>";
 }
-chat_btn10.addEventListener('click', chat_Box10);
+chat_b.addEventListener('click', chat);
